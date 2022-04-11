158161e3c74e12246e4cedc74e2c1079ac36884b
Tara Sawhney <83092957+tarasawhney@users.noreply.github.com>
2022-04-11 15:11:24 -0400

    Update connected_scatter.js

--- js/connected_scatter.js
+++ js/connected_scatter.js
@@ -13,7 +13,7 @@ const cs_svg = d3.select("#connected_scatter")
         .attr("transform", `translate(${cs_margin.left},${cs_margin.top})`);
 
 //read the data 
-d3.csv('data/citibike-data.csv').then(function(cs_data){
+d3.csv('data/end_stations').then(function(cs_data){
     // list of groups 
     const allGroup = ["Grand Army Plaza & Central Park S", "34 Ave & 38 St", "1 Ave & E 18 St"]
 
