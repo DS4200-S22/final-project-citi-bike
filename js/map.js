07f1669fd02c00cc26541f2f8d834b39c2372228
Tara Sawhney <83092957+tarasawhney@users.noreply.github.com>
2022-04-11 16:34:24 -0400

    Update map.js

--- js/map.js
+++ js/map.js
@@ -1,24 +1,29 @@
-const map_svg = d3.select("svg")   
-    map_width =  +map_svg.attr("width")
-    map_height = +map_svg.attr("height")
+// The svg
+const map_svg = d3.select("svg"),
+    map_width = +svg.attr("width"),
+    map_height = +svg.attr("height");
 
-//map and projection
+// Map and projection
 const projection = d3.geoMercator()
-    .center([2, 47])
-    .scale(980)
-    .translate([map_width/2, map_height/2])
+    .center([2, 47])                // GPS of location to zoom on
+    .scale(980)                       // This is like the zoom
+    .translate([ map_width/2, map_height/2 ])
 
-d3.json("https://raw.githubusercontent.com/dwillis/nyc-maps/master/boroughs.geojson").then(function(map_data){
-    map_data.features = map_data.features.filter(d =>{console.log(d.properties.name); return d.properties.name=="Manhattan"})
+// Load external data and boot
+d3.json("https://raw.githubusercontent.com/dwillis/nyc-maps/master/boroughs.geojson").then( function(map_data){
 
-    //draw map 
+    // Filter data
+    map_data.features = map_data.features.filter(d => {console.log(d.properties.name); return d.properties.name=="Manhattan"})
+
+    // Draw the map
     map_svg.append("g")
         .selectAll("path")
         .data(map_data.features)
         .join("path")
-            .attr("fill", "grey")
-            .attr("d", d3.geoPath()
-                .projection(projection)
-                )
-                .style("stroke", "none")
+          .attr("fill", "grey")
+          .attr("d", d3.geoPath()
+              .projection(projection)
+          )
+          .style("stroke", "none")
 })
+     
