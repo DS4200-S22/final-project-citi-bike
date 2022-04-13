1f7c8f567c58e4764b5bb0bf79a4ef218cf8ddc6
Tara Sawhney <83092957+tarasawhney@users.noreply.github.com>
2022-04-13 11:31:11 -0400

    Update pie_2.js

--- js/pie_2.js
+++ js/pie_2.js
@@ -3,17 +3,17 @@ const pie_width = 450,
     pie_height = 450, 
     pie_margin = 40, 
 
-  //radius 
-  const pie_radius = Math.min(pie_width, pie_height) / 2 - pie_margin; 
+//radius 
+const pie_radius = Math.min(pie_width, pie_height) / 2 - pie_margin; 
 
-  //append svg 
-  const pie_svg = d3.select("#pie_viz")
-    .append("svg")
-      .attr("width", pie_width)
-      .attr("height", pie_height)
+//append svg 
+const pie_svg = d3.select("#pie_viz")
+  .append("svg")
+    .attr("width", pie_width)
+    .attr("height", pie_height)
 
-    .append("g")
-      .attr("transform", `translate(${pie_width/2}, ${pie_height/2})`);
+  .append("g")
+    .attr("transform", `translate(${pie_width/2}, ${pie_height/2})`);
 
   //create data 
   const p_data_1 = {a: 53, b: 50, c: 25, d: 23, e: 21, f: 17, g: 16, h: 16, i: 16, k: 14}
