e4399293fb57236df7ee9b019089026d182ea23c
Tara Sawhney <83092957+tarasawhney@users.noreply.github.com>
2022-04-13 15:04:19 -0400

    Update new_map.js

--- js/new_map.js
+++ js/new_map.js
@@ -1,4 +1,4 @@
-let my_map = L.map("map").setView([40.7831, 73.9712], 25); 
+let my_map = L.map("map").setView([40.7831, 73.9712], 5); 
 
 L.tileLayer('https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
