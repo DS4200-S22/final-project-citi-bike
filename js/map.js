997e029490042467507c136ea8c7ab5857a8f9a2
Tara Sawhney <83092957+tarasawhney@users.noreply.github.com>
2022-04-13 11:52:22 -0400

    Update map.js

--- js/map.js
+++ js/map.js
@@ -1,9 +1,9 @@
-let map_svg = d3.select("svg")
-    map_width = +map_svg.attr("width")
-    map_height = +map_svg.attr("height")
+const map_svg_1 = d3.select("svg")
+    map_width = +map_svg_1.attr("width")
+    map_height = +map_svg_1.attr("height")
 
 //map and projection 
-let map_projection = d3.geoMercator()
+const map_projection = d3.geoMercator()
     .center([2, 47])
     .scale(980)
     .translate([map_width/2, map_height/2])
@@ -14,7 +14,7 @@ d3.json("https://raw.githubusercontent.com/dwillis/nyc-maps/master/boroughs.geoj
     map_data.features = map_data.features.filter(d => {console.log(d.properties.name); return d.properties.name=="Manhattan"})
 
     //draw the map 
-    map_svg.append("g")
+    map_svg_1.append("g")
         .selectAll("path")
         .data(map_data.features)
         .join("path")
