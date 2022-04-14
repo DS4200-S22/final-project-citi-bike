501ea58eec738ba576ede8bec98dec25a980c880
skydeboer <33607687+skydeboer@users.noreply.github.com>
2022-04-13 20:20:26 -0400

    Update new_map.js

--- js/new_map.js
+++ js/new_map.js
@@ -1,11 +1,12 @@
 
 let my_map = L.map("map").setView([40.7831, -73.984016], 12); 
 
-L.tileLayer('https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png', {
+let layer = new L.tileLayer('https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
     // Attribution is obligatory as per copyright!
     maxZoom: 20
-}).addTo(my_map);
+});
+my_map.addLayer(layer);
 
 //markers for start stations
 let ss_marker_1 = L.marker([40.72621788, -73.98379855]).addTo(my_map); 
