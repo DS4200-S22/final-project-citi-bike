1591f80090ef29575222523568f280dd866de3e8
Tara Sawhney <83092957+tarasawhney@users.noreply.github.com>
2022-04-18 13:24:51 -0400

    Update new_map.js

--- js/new_map.js
+++ js/new_map.js
@@ -29,16 +29,16 @@ let ss_marker_10 = L.marker([40.719383, -73.991479]).addTo(my_map);
 
 
 
-ss_marker_1.bindPopup("<b> E 7 & Avenue A, Starting Rides: 53, Riders: 45 Member, 8 Casual</b>");
-ss_marker_2.bindPopup("<b> 5 Ave & E 72 St, Starting Rides: 50, Riders: 3 Members, 47 Casual</b>");
-ss_marker_3.bindPopup("<b> 34 Ave & 31 St, Starting Rides: 25, Riders: 13 Members, 12 Casual</b>");
-ss_marker_4.bindPopup("<b> Bond St & Fulton St, Starting Rides: 23, Riders: 17 Members, 6 Casual </b>");
-ss_marker_5.bindPopup("<b> W 84 St & Broadway, Starting Rides: 21, Riders: 20 Members, 1 Casual </b>");
-ss_marker_6.bindPopup("<b> Allen St & Hester St', 'Starting Rides: 17', 'Riders: 9 Members, 8 Casual </b>"); 
-ss_marker_7.bindPopup("<b> Kane St & Clinton St', 'Starting Rides: 16', 'Riders: 7 Members, 9 Casual </b>"); 
-ss_marker_8.bindPopup("<b> W 100 St & Manhattan Ave', 'Starting Rides: 16', 'Riders: 13 Members, 3 Casual </b>");
-ss_marker_9.bindPopup("<b> 6 Ave & Broome St', 'Starting Rides: 16', 'Riders: 13 Members, 3 Casual </b>"); 
-ss_marker_10.bindPopup("<b> Delancey St & Eldridge St', 'Starting Rides: 14', 'Riders: 2 Members, 12 Casual </b>"); 
+ss_marker_1.bindPopup("<b> Street Name: E 7 & Avenue A, Starting Rides: 53, Riders: 45 Member, 8 Casual</b>");
+ss_marker_2.bindPopup("<b> Street Name: 5 Ave & E 72 St, Starting Rides: 50, Riders: 3 Members, 47 Casual</b>");
+ss_marker_3.bindPopup("<b> Street Name: 34 Ave & 31 St, Starting Rides: 25, Riders: 13 Members, 12 Casual</b>");
+ss_marker_4.bindPopup("<b> Street Name: Bond St & Fulton St, Starting Rides: 23, Riders: 17 Members, 6 Casual </b>");
+ss_marker_5.bindPopup("<b> Street Name; W 84 St & Broadway, Starting Rides: 21, Riders: 20 Members, 1 Casual </b>");
+ss_marker_6.bindPopup("<b> Street Name: Allen St & Hester St', 'Starting Rides: 17', 'Riders: 9 Members, 8 Casual </b>"); 
+ss_marker_7.bindPopup("<b> Street Name: Kane St & Clinton St', 'Starting Rides: 16', 'Riders: 7 Members, 9 Casual </b>"); 
+ss_marker_8.bindPopup("<b> Street Name: W 100 St & Manhattan Ave', 'Starting Rides: 16', 'Riders: 13 Members, 3 Casual </b>");
+ss_marker_9.bindPopup("<b> Street Name: 6 Ave & Broome St', 'Starting Rides: 16', 'Riders: 13 Members, 3 Casual </b>"); 
+ss_marker_10.bindPopup("<b> Street Name: Delancey St & Eldridge St', 'Starting Rides: 14', 'Riders: 2 Members, 12 Casual </b>"); 
 
 
 let es_marker_1 = L.marker([40.75500254, -73.98014437], {icon: greenIcon}).addTo(my_map); 
@@ -53,16 +53,17 @@ let es_marker_9 = L.marker([40.68539567, -73.97431458], {icon: greenIcon}).addTo
 let es_marker_10 = L.marker([40.70112, -73.93039], {icon: greenIcon}).addTo(my_map); 
 
 
-es_marker_1.bindPopup("<b> Grand Army Plaza and Central Park South', 'Ending Rides: 105', 'Riders: 63 Casual</b>");
-es_marker_2.bindPopup("<b> 34 Ave & 38 St','Ending Rides: 50', 'Riders: 31 Members, 19 Casual</b>");
-es_marker_3.bindPopup("<b> 1 Ave & E 18 St','Ending Rides: 50', 'Riders: 34 Members, 15 Casual</b>");
-es_marker_4.bindPopup("<b> Clinton St & Joralemon St','Ending Rides: 47', 'Riders: 30 Members, 17 Casual </b>");
-es_marker_5.bindPopup("<b> MacDougal St & Prince St','Ending Rides: 43', 'Riders: 28 Members, 15 Casual </b>");
-es_marker_6.bindPopup("<b> W 16 St & The High Line','Ending Rides: 38', 'Riders: 24 Members, 14 Casual </b>"); 
-es_marker_7.bindPopup("<b> W 47 & 6 Ave','Ending Rides: 36', 'Riders: 16 Members, 20 Casual </b>"); 
-es_marker_8.bindPopup("<b> E 11 & Avenue B','Ending Rides: 34', 'Riders: 25 Members, 9 Casual </b>"); 
-es_marker_9.bindPopup("<b> Baltic St & 5 Ave','Ending Rides: 24', 'Riders: 12 Members, 0 Casual </b>"); 
-es_marker_10.bindPopup("<b> Prospect Pl & 6 Ave','Ending Rides: 19', 'Riders: 10 Members, 0 Casual </b>"); 
+es_marker_1.bindPopup("<b> Street Name: Grand Army Plaza and Central Park South', 'Ending Rides: 105', 'Riders: 63 Casual</b>");
+es_marker_2.bindPopup("<b> Street Name: 34 Ave & 38 St','Ending Rides: 50', 'Riders: 31 Members, 19 Casual</b>");
+es_marker_3.bindPopup("<b> Street Name: 1 Ave & E 18 St','Ending Rides: 50', 'Riders: 34 Members, 15 Casual</b>");
+es_marker_4.bindPopup("<b> Street Name: Clinton St & Joralemon St','Ending Rides: 47', 'Riders: 30 Members, 17 Casual </b>");
+es_marker_5.bindPopup("<b> Street Name: MacDougal St & Prince St','Ending Rides: 43', 'Riders: 28 Members, 15 Casual </b>");
+es_marker_6.bindPopup("<b> Street Name: W 16 St & The High Line','Ending Rides: 38', 'Riders: 24 Members, 14 Casual </b>"); 
+es_marker_7.bindPopup("<b> Street Name: W 47 & 6 Ave','Ending Rides: 36', 'Riders: 16 Members, 20 Casual </b>"); 
+es_marker_8.bindPopup("<b> Street Name: E 11 & Avenue B','Ending Rides: 34', 'Riders: 25 Members, 9 Casual </b>"); 
+es_marker_9.bindPopup("<b> Street Name: Baltic St & 5 Ave','Ending Rides: 24', 'Riders: 12 Members, 0 Casual </b>"); 
+es_marker_10.bindPopup("<b> Street Name: Prospect Pl & 6 Ave','Ending Rides: 19', 'Riders: 10 Members, 0 Casual </b>"); 
+
 
 let legend = L.control({position: "bottomleft"}); 
 legend.onAdd = function(my_map) {
@@ -77,5 +78,4 @@ legend.onAdd = function(my_map) {
 
 }; 
 
-legend.addTo(my_map);  
-
+legend.addTo(my_map); 
